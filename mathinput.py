a6909fe11913534a5df2574ea0d382251fe19098
theotms <theo.mendes@hotmail.com>
2023-08-29 13:25:21 +0300

    Théo

---
+++ mathinput.py
@@ -0,0 +1,5 @@
+acceleration = float(input("Enter the value of acceleration in m/s²: "))
+mass = float(input("Enter the mass if the object in Kg: "))
+weight = float(acceleration * mass)
+
+print(f"The weight is {weight:.2f}N")
